cac49744cd8b48e7555cee779314ac7b4c6d8d53
Natanael Silva <natanaelsilva.cc16@gmail.com>
2025-05-17 17:02:03 -0300

    Add repositories

---
+++ src/core/exceptions.py
@@ -0,0 +1,14 @@
+class DuplicateEmailError(Exception):
+    pass
+
+
+class ClientNotFoundError(Exception):
+    pass
+
+
+class ProductNotFoundError(Exception):
+    pass
+
+
+class DuplicateFavoriteError(Exception):
+    pass
---
+++ src/infrastructure/database/repositories/client.py
@@ -0,0 +1,27 @@
+from abc import ABC, abstractmethod
+
+from pydantic import UUID4
+
+from core.entities.client import Client
+
+
+class ClientRepository(ABC):
+    @abstractmethod
+    async def create(self: 'ClientRepository', client: Client) -> Client:
+        pass
+
+    @abstractmethod
+    async def get_by_id(self: 'ClientRepository', client_id: UUID4) -> Client | None:
+        pass
+
+    @abstractmethod
+    async def get_by_email(self: 'ClientRepository', email: str) -> Client | None:
+        pass
+
+    @abstractmethod
+    async def update(self: 'ClientRepository', client: Client) -> Client:
+        pass
+
+    @abstractmethod
+    async def delete(self: 'ClientRepository', client_id: UUID4) -> None:
+        pass
